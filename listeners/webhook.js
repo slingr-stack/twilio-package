30171598636d93257c629f194d50cdf2d5e5d035
germanm98 <germanmanzur98@gmail.com>
2025-03-09 22:22:24 -0300

    updating documentation and api.js file

--- listeners/webhook.js
+++ listeners/webhook.js
@@ -13,17 +13,7 @@ listeners.defaultWebhookTwilio = {
         }
     },
     callback: function(event) {
-        sys.logs.info('Received Twilio webhook. Processing and triggering a package event.');
-        var body = JSON.stringify(event.data.body);
-        var params = event.data.parameters;
-        if(true) {
-            sys.logs.info('Valid webhook received. Triggering event.');
-            sys.events.triggerEvent('twilio:webhook', {
-                body: body,
-                params: params
-            });
-            return "ok";
-        }
-        else throw new Error("Invalid webhook");
+        sys.logs.info('[twilio] Received Twilio webhook. Processing and triggering a package event.', event);
+        sys.events.triggerEvent('twilio:webhook', event.data);
     }
 };
--- scripts/api.js
+++ scripts/api.js
@@ -35,6 +35,30 @@ for (let key in httpDependency) {
 }
 
 /****************************************************
+Helpers
+****************************************************/
+
+exports.messages = {};
+
+/**
+ * Sends an HTTP GET request to the specified URL with the provided HTTP options.
+ *
+ * @param {string} to       - The phone number the SMS is sent to.
+ * @param {string} from     - The phone number the SMS is sent from.
+ * @param {string} body     - The actual SMS content.
+ * @return {object}         - The response of the POST request.
+ */
+
+exports.messages.sendSMS = function(to, from, body) {
+    const accountSid = pkg.twilio.utils.getConfiguration("accountSid");
+    return pkg.twilio.api.post(`/Accounts/${accountSid}/Messages.json`, {
+        To: to,
+        From: from,
+        Body: body
+    });
+};
+
+/****************************************************
  Public API - Generic Functions
  ****************************************************/
 
@@ -81,20 +105,6 @@ exports.put = function(path, httpOptions, callbackData, callbacks) {
 };
 
 /**
- * Sends an HTTP PATCH request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the PATCH request to.
- * @param {object} httpOptions  - The options to be included in the PATCH request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the POST request. [optional]
- * @return {object}             - The response of the PATCH request.
- */
-exports.patch = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.patch(Twilio(options), callbackData, callbacks);
-};
-
-/**
  * Sends an HTTP DELETE request to the specified URL with the provided HTTP options.
  *
  * @param {string} path         - The path to send the DELETE request to.
@@ -106,97 +116,6 @@ exports.patch = function(path, httpOptions, callbackData, callbacks) {
 exports.delete = function(path, httpOptions, callbackData, callbacks) {
     let options = checkHttpOptions(path, httpOptions);
     return httpService.delete(Twilio(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP HEAD request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the HEAD request to.
- * @param {object} httpOptions  - The options to be included in the HEAD request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the HEAD request. [optional]
- * @return {object}             - The response of the HEAD request.
- */
-exports.head = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.head(Twilio(options), callbackData, callbacks);
-};
-
-/**
- * Sends an HTTP OPTIONS request to the specified URL with the provided HTTP options.
- *
- * @param {string} path         - The path to send the OPTIONS request to.
- * @param {object} httpOptions  - The options to be included in the OPTIONS request check http-service documentation.
- * @param {object} callbackData - Additional data to be passed to the callback functions. [optional]
- * @param {object} callbacks    - The callback functions to be called upon completion of the OPTIONS request. [optional]
- * @return {object}             - The response of the OPTIONS request.
- */
-exports.options = function(path, httpOptions, callbackData, callbacks) {
-    let options = checkHttpOptions(path, httpOptions);
-    return httpService.options(Twilio(options), callbackData, callbacks);
-};
-
-exports.utils = {
-
-    /**
-     * Converts a given date to a timestamp.
-     *
-     * @param {number | string} params      - The date to be converted.
-     * @return {object}                     - An object containing the timestamp.
-     */
-    fromDateToTimestamp: function(params) {
-        if (!!params) {
-            return {timestamp: new Date(params).getTime()};
-        }
-        return null;
-    },
-
-    /**
-     * Converts a timestamp to a date object.
-     *
-     * @param {number} timestamp            - The timestamp to convert.
-     * @return {object}                     - The date object representing the timestamp.
-     */
-    fromTimestampToDate: function(timestamp) {
-        return new Date(timestamp);
-    },
-
-    /**
-     * Gets a configuration from the properties.
-     *
-     * @param {string} property             - The name of the property to get.
-     *                                          If it is empty, return the entire configuration object.
-     * @return {string}                     - The value of the property or the whole object as string.
-     */
-    getConfiguration: function (property) {
-        if (!property) {
-            sys.logs.debug('[twilio] Get configuration');
-            return JSON.stringify(config.get());
-        }
-        sys.logs.debug('[twilio] Get property: '+property);
-        return config.get(property);
-    },
-
-    /**
-     * Concatenates a path with a param query and its value.
-     *
-     * @param path                          - The path to concatenate.
-     * @param key                           - The name of the param.
-     * @param value                         - The value of the param.
-     * @returns {string}                    - The concatenated path without coding parameters.
-     */
-    concatQuery: function (path, key, value) {
-        return path + ((!path || path.indexOf('?') < 0) ? '?' : '&') + key + "=" + value;
-    },
-
-    /**
-     * Merges two JSON objects into a single object.
-     *
-     * @param {Object} json1 - The first JSON object to be merged.
-     * @param {Object} json2 - The second JSON object to be merged.
-     * @return {Object} - The merged JSON object.
-     */
-    mergeJSON: mergeJSON,
 };
 
 /****************************************************
