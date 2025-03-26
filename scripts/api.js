33db22560d8bf70b13c42cbd756339a199cf09b1
germanm98 <germanmanzur98@gmail.com>
2025-03-26 12:03:29 -0300

    fixing changes requested

--- scripts/api.js
+++ scripts/api.js
@@ -42,6 +42,7 @@ Helpers
  * @param {string} body     - The actual SMS content.
  * @return {object}         - The response of the POST request.
  */
+
 exports.sendSMS = function(to, from, body) {
     const accountSid = config.get("accountSid");
     let options = checkHttpOptions(`/Accounts/${accountSid}/Messages.json`, { body: {
